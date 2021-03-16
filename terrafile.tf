11bb31aa0f2dde7818b0651876a8cfca97bb2213
Pedro DevOps <pedrodevops@gmail.com>
2021-03-16 20:20:39 -0300

    Mudar a chamada do módulo para criar duas ec2

--- terrafile.tf
+++ terrafile.tf
@@ -1,23 +1,32 @@
+# Esse arquivo faz a chamada do módulo que criar ec2 na AWS passando parametros
+# O arquivo atual possui mais inteligencia do negócio enquanto o módulo é mais genérico
+# O módulo pode ser abstraído como uma classe e nesse arquivo instanciamos o objeto
+
+#Provider que será utilizado será a AWS
 provider "aws" {
   region  = "us-east-2"
   version = "~> 3.0"
 }
 
-terraform {
+# Se quiser colocar o state em um bucket s3 da AWS descomente o bloco a seguir
+/*terraform {
   backend "s3" {
     # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
     bucket = "descomplicando-terraform-pedro-bucket-01"
     key    = "terraform-test.tfstate"
     region = "us-east-2"
   }
-}
+}*/
 
+# Módulo que cria instancias ec2 na AWS
+# Utilizamos um for_each nessa chamada para forçar a execução de quantas vezes for necessária
 module "ec2" {
   source                  = "git@github.com:PedroDevOps/descomplicando_terraform_module.git?ref=v0.2"
-  app_name                = "ec2_app"
-  instance_type           = "t2.micro"
+  app_name                = each.value.app_name
+  instance_type           = each.value.instance_type
+  for_each                = var.projeto
 }
 
 output "ip_address_ec2" {
-  value = module.ec2.ip_address
+  value = values(module.ec2)[*].ip_address
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
-variable "hash_commit" {
-  default = "806d52dafe9b7fddbc4f0d2d41086ed3cfa02a44"
-}
+variable "projeto" {
+  description = "Mapa de Projetos"
+  type = "map"
 
-variable "app_name" {
-  type        = string
-  default     = "Sem nome"
-  description = "Nome da app"
-}
-
-variable "instance_type" {
-  type = string
-  default = "t2.micro"
-  description = "Tipo de instância ec2 usada na AWS"
+  default = {
+    projeto1 = {
+      instance_type = "t2.micro"
+      app_name = "projeto1"
+    },
+    projeto2 = {
+      instance_type = "t3.medium"
+      app_name = "projeto2"
+    }
+  }
 }
